5427c90c9be7e293d4f070c797c16f54fa7802df
VonRehberg <vonrehberg88@gmail.com>
2017-09-17 18:51:14 +0200

    first implementation for english version

--- index.js
+++ index.js
@@ -1,172 +1,33 @@
 var Alexa = require('alexa-sdk');
 
-var reprompts = [
-  "Möchtest du mehr",
-  "Magst du mehr <phoneme alphabet='ipa' ph='\"d3`ti tOk'>Dirty Talk</phoneme>",
-  "Willst du noch mehr Schweinereien",
-  "Soll ich dich weiter bezirzen",
-  "Wollen wir uns weiter amüsieren",
-  "Erträgst du noch mehr",
-  "Sollen wir fortfahren",
-  "Haben wir Zeit für mehr",
-  "Kannst du noch was aushalten",
-  "Gefällt dir das",
-  "Magst du das"
-];
+var goodByMessage,
+    helpMessage,
+    introduction,
+    reprompts,
+    dirtyMessages,
+    pronomen,
+    Anrede;
 
-var dirtyMessages = [
-  "Du bekommst gleich den Aal abgezogen,",
-  "Ich versohl dir den Hintern,",
-  "Du machst mich ganz <phoneme alphabet='ipa' ph='ˈvʊʃɪç'>wuschig</phoneme>,",
-  "Ich schüttel dir die Palme,",
-  "Ich butter dir dein Brötchen,",
-  "Ich bin spitz wie Nachbars Lumpi,",
-  "Butter mir mein Brötchen,",
-  "Ich zieh dir gleich die Pfeife trocken,",
-  "Hinten ist die Henne fett,",
-  "Putz mir die Perle,",
-  "Ich schnäuz dir den Kasper,",
-  "Dein Lachs gehört gebuttert,",
-  "Lass mich deine Lewinsky sein,",
-  "Ich liebe es Eier auszublasen,",
-  "Lass mich von deinem Nektar kosten,",
-  "Komm mit mir in den braunen Salon,",
-  "Ich möchte deinen Jürgen würgen,",
-  "Vernasch mein Dörrobst,",
-  "Gib mir Tiernamen,",
-  "Ich möchte deinen Maiskolben grillen,",
-  "Kratz' mich, beiss' mich, gib mir Tiernamen,",
-  "Setz dich auf meinen Dorn,",
-  "Lass uns dem Storch arbeit geben,",
-  "Giess Sahne in meinen Kaffee,",
-  "Giess mir Sahne in meinen Kaffee,",
-  "Es ist mal wieder Zeit für ein Mokkastößchen,",
-  "Wir müssen mal wieder horizontalen Tango tanzen,",
-  "Ich will körperlich kollidieren,",
-  "Ich schüttel dir gleich am Baum,",
-  "Lass uns Rohre verlegen,",
-  "Es ist Zeit für ein Flötenkonzert,",
-  "Zieh mich über deinen Docht,",
-  "Zieh mich über deinen Kolben,",
-  "Fütter meine Schnecke,",
-  "Ich will Flöte lernen,",
-  "Bürste mir den Ofen aus,",
-  "Ich hab Lust auf Herrensahne,",
-  "Jetzt wird erst mal richtig abgesahnt,",
-  "Ab in die Mokkabude,",
-  "Leck mich am Zückerli,",
-  "Wasch mir die Wanne aus,",
-  "Streich mir die Kerze,",
-  "Lass mich deine kleinen Schwimmer befreien,",
-  "Ich hau dir auf den Spitz,",
-  "Lass uns in den bewaldeten Süden vorstoßen,",
-  "Wir müssen die Schlange verstecken,",
-  "Ich hab lust auf Schiffe versenken,",
-  "Ich möchte auf dem dritten Bein stehen,",
-  "Es ist Zeit, dass du auf der G-Saite spielst,",
-  "Du willst mich wohl aufs Kreuz legen,",
-  "Ich will das Besteck in das Silberbad tunken,",
-  "Such das Blümchen,",
-  "Lade mich auf deinen Besen,",
-  "Wo ist denn das Stehaufmännchen?",
-  "Ich will den Aal zucken lassen,",
-  "Der Acker muss mal wieder gepflügt werden,",
-  "Steck den Kopf ins Loch,",
-  "Ich putz dir den Lauf,",
-  "Heiz meinen Ofen,",
-  "Lass mal deinen Specht hacken,",
-  "Lass deinen Vize singen,",
-  "Schwenk die Fahne,",
-  "Ich geige dir gleich was,",
-  "Rühr mir im Fleischtopf,",
-  "Wir sollten nach Öl bohren,",
-  "Giess mir Öl in die Lampe,",
-  "Rühr mich schaumig,",
-  "Niess mir unters Kinn,"
-];
-
-var pronomen = ["du", "mein", "mein"];
-var Anrede = [
-  "Strolch",
-  "Jeck",
-  "Schwerenöter",
-  "Doktor",
-  "Meister",
-  "Chef",
-  "Liebling",
-  "Honey",
-  "Baby",
-  "Darling",
-  "Cheri",
-  "Süßer",
-  "Seestern",
-  "Katerchen",
-  "Prinz",
-  "Boss",
-  "König",
-  "Sklave",
-  "starker Mann",
-  "Schwächling",
-  "Delfin",
-  "böser Delfin",
-  "Kasper",
-  "kleiner Wicht",
-  "Sweetheart",
-  "Sugar",
-  "Frechdachs",
-  "Adonis",
-  "Amor",
-  "Captain",
-  "Casanova",
-  "Champ",
-  "Cheffkoch",
-  "Dreamboy",
-  "Flußpferd",
-  "Goldstück",
-  "Gott",
-  "Göttergatte",
-  "Großer",
-  "Hero",
-  "König",
-  "Lover",
-  "Moppelchen",
-  "Ömmelchen",
-  "Polarstern",
-  "Prinz",
-  "Purzel",
-  "Puschelchen",
-  "Pusteblume",
-  "Quarktörtchen",
-  "Rackerchen",
-  "Schäfchen",
-  "Schaumprinz",
-  "Schöner",
-  "Sexy",
-  "Sternenstaub",
-  "Strahlemann",
-  "Sunny",
-  "Sunnyboy",
-  "Sunshine",
-  "Teufelchen",
-  "Tiger",
-  "Träumer",
-  "Traumprinz",
-  "Undercoverengel",
-  "Urmel",
-  "Veilchen",
-  "Vulkan",
-  "Waffeleisenprinz",
-  "wilder Tiger",
-  "Yuppie",
-  "Zauberer",
-  "Zauberwesen",
-  "Zeus",
-  "Zimttiger",
-  "Zorro",
-  "Zottel",
-  "Otto",
-  "Zwergbüffel"
-];
+function loadLocale(sLocale) {
+  var oLocale;
+  switch(sLocale) {
+    case "de-DE":
+      oLocale = require("./locales/de.js");
+      break;
+    case "en-US":
+    case "en-GB":
+    default:
+      oLocale = require("./locales/en.js");
+      break;
+  }
+  goodByMessage = oLocale.goodByMessage;
+  helpMessage = oLocale.helpMessage;
+  introduction = oLocale.introduction;
+  reprompts = oLocale.reprompts;
+  dirtyMessages = oLocale.dirtyMessages;
+  pronomen = oLocale.pronomen;
+  Anrede = oLocale.Anrede;
+}
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -190,7 +51,7 @@ function doDirtyTalk(message) {
 }
 
 function startDirtyTalk() {
-  this.emit(':ask', "Wer war böse?", getReprompt());
+  this.emit(':ask', introduction, getReprompt());
 }
 
 var handlers = {
@@ -210,7 +71,7 @@ var handlers = {
       doDirtyTalk.call(this, getDirtyTalkMessage());
     },
     'CloseSession': function() {
-      this.emit(':tell', "Bis zum nächsten mal. Du kleiner Schmutzfink");
+      this.emit(':tell', goodByeMessage);
     },
     'AMAZON.CancelIntent': function() {
       this.emit("CloseSession");
@@ -219,11 +80,12 @@ var handlers = {
       this.emit("CloseSession");
     },
     'AMAZON.HelpIntent': function() {
-      this.emit(":ask", "Starte den Skill mit dem Kommando 'Starte Dirty Talk'. Zwischen den Ansagen kannst du mit beliebigen Ansagen wie zum Beispiel 'Mehr' oder 'Weiter' nach weiteren Sprüchen verlangen. Möchtest du jetzt etwas Dirty Talk, du kleiner Schwerenöter?");
+      this.emit(":ask", helpMessage);
     }
  };
 
  exports.handler = function(event, context, callback) {
+      loadLocale(event.request.locale);
       var alexa = Alexa.handler(event, context);
       alexa.registerHandlers(handlers);
       alexa.execute();
---
+++ locales/en.js
@@ -0,0 +1,71 @@
+var goodByMessage = "See you next time. My little mucky pup!";
+var helpMessage = "Open the skill by saying 'Start dirty talk'. Between the different phrases you can ask for more dirty talk by saying 'more' or 'yes'. Would you like to start some dirty talk right now, you filthy beggar?";
+var introduction = "Who was nasty?";
+
+var reprompts = [
+    "Do you like that?",
+    "Do you want more?",
+    "You want some more?"
+  ];
+  
+  var dirtyMessages = [
+    "Let me butter your bread,",
+    "I will butter your bread,",
+    "Let me be your Lewinsky,",
+    "Toss my salad,",
+    "Roses are okay. Violets are fine. You be the 6. And I'll be the 9,",
+    "Roses are red. Violets are blue. Your webcam is on. And I'm watching you,",
+    "I want you to knock on my backdoor,",
+    "I'm gonna come to your house and knock knock at your backdoor,",
+    "I'm going to Chris Brown you,",
+    "I will wax your pole,",
+    "Let me wax your clown,",
+    "Let me milk your goat",
+    "Paddle my pink canoe,",
+    "Please take your talents to south beach,",
+    "Please walk my cat,",
+    "Let me toot your horn,",
+    "I'm going to choke your chicken,",
+    "Let me bash your bishop,",
+    "I will make your bald man cry,",
+    "I'm going to adjust your antenna,",
+    "May I shake hands with your president,",
+    "Churn my butter,",
+    "Attack my pink fortress,",
+    "Let's bam bam in the ham,",
+    "Bend me over a barrel and show me the fifty states,",
+    "Check my oil,",
+    "Fidget the midget in bridget,",
+    "Fill my gas tank,",
+    "Let's go crab fishing in the dead sea,",
+    "Let's take the old dirt trail,",
+    "Would you mind going down my Hershey Highway,",
+    "I want you to race at the chocolate speedway,",
+    "I opened my gates of Mordor for you,",
+    "Let's pray with the knees upwards,",
+    "Put condensed milk on my waffle,",
+    "Put ranch dressing in Hidden Valley,",
+    "Shoot the meat rocket into my sausage wallet,",
+    "Let's sour the kraut,",
+    "Let's take the bald-headed gnome for a stroll in the misty forest,",
+    "Tickle my tummy from the inside,",
+    "Stuff my turkey,"
+  ];
+  
+  var pronomen = ["my", ""];
+  var Anrede = [
+    "princess",
+    "darling",
+    "honey",
+    "sugar"
+  ];
+
+module.exports = {
+  goodByMessage: goodByMessage,
+  helpMessage: helpMessage,
+  introduction: introduction,
+  reprompts: reprompts,
+  dirtyMessages: dirtyMessages,
+  pronomen: pronomen,
+  Anrede: Anrede
+};
